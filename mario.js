c68588eee0f59c97d7384dba2321710deec387bd
Lijo Thomas <Lijo Thomas>
2017-03-06 20:11:48 -0500

    finished mario

--- mario.js
+++ mario.js
@@ -1,4 +1,3 @@
-
 printPyramid(5);
 
 
@@ -13,11 +12,19 @@ printPyramid(5);
  *       #####
  *      ######
  */
-function printPyramid(height) {
-    console.log("Uh oh... the pyramid is under construction.");
-    console.log("Check back soon, our developers are hard at work as we speak!");
+//function printPyramid(height) {
+//    console.log("Uh oh... the pyramid is under construction.");
+//    console.log("Check back soon, our developers are hard at work as we speak!");
 
     // TODO
     // print that pyramid!
+//}
+function printPyramid(height) {
+for (var brick = "##"; brick.length <= height + 1; brick+="#")
+{
+  for(var blank=" "; blank.length <= height - brick.length + 1; blank += " ")
+  {}
 
+    console.log(blank+brick)
+  }
 }
