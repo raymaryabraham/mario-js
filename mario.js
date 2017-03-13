67bc0b27344050639ed9c9f0876f6ab83dc29884
Lijo Thomas <Lijo Thomas>
2017-03-13 19:28:55 -0400

    allow user to input pyramid height and respond to button click

--- mario.js
+++ mario.js
@@ -8,24 +8,26 @@
 function determineHeightAndThenDrawPyramid() {
 
     // just so we know we're here
-    console.log("someone invoked the determineHeightAndThenDrawPyramid function!");
 
     // TODO 3
     // figure out the height the user typed (replace the "5" below)
-    heightStr = "5";
+  heightStr=document.getElementById("height").value
 
     // here we convert the string to an int
     height = parseInt(heightStr);
 
     // TODO 2
     // draw the pyramid with the given height
+    drawPyramid(height);
 
 }
 
 
 // TODO 1
 // hook up the button's click event to our determineHeightAndThenDrawPyramid function
-
+document.getElementById("mybutton").addEventListener("click", function(){
+           determineHeightAndThenDrawPyramid();
+  });
 
 
 /**
@@ -38,7 +40,7 @@ function determineHeightAndThenDrawPyramid() {
      // TODO 4
      // before drawing, clear the old content
 
-
+document.getElementById("pyramid").innerHTML = "";
      // for each row....
      for (var row = 0; row < height; row++) {
 
